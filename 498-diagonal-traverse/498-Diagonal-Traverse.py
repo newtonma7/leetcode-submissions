9ebd2d745c6b9dd75d5148b33e368c32dabb29c8
newtonma7 <123204560+newtonma7@users.noreply.github.com>
2026-08-27 18:33:07 -0400

    [498] [Time Beats: 97.57%] [Memory Beats: 40.42%] - LeetPush

--- 498-diagonal-traverse/498-Diagonal-Traverse.py
+++ 498-diagonal-traverse/498-Diagonal-Traverse.py
@@ -2,36 +2,34 @@ class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         rows = len(mat)
         cols = len(mat[0])
+        cur_row = cur_col = 0
         n = rows * cols
         ans = []
-        curr_row = curr_col = 0
         up = True
 
         while len(ans) != n:
             if up:
-                while curr_row >= 0 and curr_col < cols:
-                    ans.append(mat[curr_row][curr_col])
-
-                    curr_row -= 1
-                    curr_col += 1
-
-                if curr_col == cols:
-                    curr_row += 2
-                    curr_col -= 1
+                while cur_row >= 0 and cur_col < cols:
+                    ans.append(mat[cur_row][cur_col])
+                    cur_row -=1
+                    cur_col +=1
+                
+                if cur_col == cols:
+                    cur_row += 2
+                    cur_col -= 1
                 else:
-                    curr_row +=1
+                    cur_row +=1
                 up = False
             else:
-                while curr_row < rows and curr_col >= 0:
-                    ans.append(mat[curr_row][curr_col])
-
-                    curr_row += 1
-                    curr_col -= 1
-
-                if curr_row == rows:
-                    curr_row -= 1
-                    curr_col += 2
+                while cur_row < rows and cur_col >= 0:
+                    ans.append(mat[cur_row][cur_col])
+                    cur_row +=1
+                    cur_col -=1
+                    
+                if cur_row == rows:
+                    cur_row -=1
+                    cur_col +=2
                 else:
-                    curr_col += 1
+                    cur_col += 1
                 up = True
         return ans
